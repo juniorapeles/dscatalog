9f9b99d004ecb278f5501aa7576dc89a11ddb0ac
eli <elijunior.apeles2005@gmail.com>
2024-03-14 21:09:54 -0300

    feat - Category repository created, h2, service

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -2,12 +2,13 @@ package com.devsuperior.dscatalog.resources;
 
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,12 +16,12 @@ import java.util.List;
 @RequestMapping(value = "/categories")
 public class CategoryResource{
 
+    @Autowired
+    CategoryService service;
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "Books"));
-        list.add(new Category(1L, "Eletronics"));
-
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
